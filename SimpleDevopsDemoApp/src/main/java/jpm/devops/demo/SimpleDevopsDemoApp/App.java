16d0beaaad1eee7611dad1fd9b3fa159dfe70f19
Ilakkiaselvan Subbiah <ilakkiaselvan@users.norely.github.com>
2017-06-10 02:33:22 +0530

    Initial commit

---
+++ SimpleDevopsDemoApp/src/main/java/jpm/devops/demo/SimpleDevopsDemoApp/App.java
@@ -0,0 +1,13 @@
+package jpm.devops.demo.SimpleDevopsDemoApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
